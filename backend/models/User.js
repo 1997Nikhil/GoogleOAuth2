ce4138a392f414eca24fa26df916bd5e4eda82a0
Nikhil Dadhich <nikhildadhich58@gmail.com>
2026-08-26 17:15:32 +0530

    Adding db.js

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+
+    profilePicture: {
+      type: String,
+    },
+
+    provider: {
+      type: String,
+      enum: ["local", "google"],
+      default: "google",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
